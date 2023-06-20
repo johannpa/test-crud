0dc385c836fda0d4686efe40cfcd9ebfed50f661
johannpa <johann_paque@hotmail.fr>
2023-06-20 16:01:30 +0200

    Add commentService into user.module in provider to use a fonction specificaly

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+
+
+    findUserComments(userId:string){
+        return 'This is the comments of the user';
+    }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { CreateUserDto } from "./dto/createUserDto";
 import { UserService } from './user.service';
+import { CommentService } from 'src/comment/comment.service';
 
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService:UserService){}
+    constructor(private readonly userService:UserService, private readonly commentService: CommentService){}
     @Get(':id')
     findOne(@Param("id") id: string) {
         return this.userService.findOne(id);
@@ -16,7 +17,8 @@ export class UserController {
         return this.userService.create(createUserDto);
     }
 
-    // @Patch()
-    // @Put()
-    // @Delete()
+    @Get(":id/comments")
+    getUserComment(@Param("id") id:string){
+        return this.commentService.findUserComments(id);
+    }
 }
